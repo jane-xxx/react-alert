e9bc86a11d2ee0eab85346cc640f5d1df56ab1df
Jane <1519763218@qq.com>
2020-09-21 22:41:01 +0800

    react-alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Alert extends Component {
+    render() {
+        return (
+            <div>
+                我是alert
+            </div>
+        )
+    }
+}
